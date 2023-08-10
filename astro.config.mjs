2f7945b334aee87651c3937540b79143ff793fd6
Timo <44401485+Timo972@users.noreply.github.com>
2023-08-10 15:29:12 +0200

    install svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thrivemedia.art",
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()]
 });
