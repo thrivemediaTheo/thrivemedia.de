c1d7f6945537f652036777e1b4d986251e6ab784
Timo <44401485+Timo972@users.noreply.github.com>
2023-08-10 13:58:31 +0200

    add bebas & mont heavy custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "rgb(53, 53, 53)",
+        text: "#edf0f1",
+        primary: "rgb(255, 213, 0)",
+      },
+      fontFamily: {
+        header: ["Mont-Heavy"],
+        body: ["Bebas"],
+      },
+    },
+  },
+  plugins: [],
+};
