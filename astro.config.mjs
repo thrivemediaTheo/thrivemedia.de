af6370f205f09d00ddb9d469b211d53c233551ef
Timo <44401485+Timo972@users.noreply.github.com>
2023-08-13 17:22:17 +0200

    img performance improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://thrivemedia.art",
-  integrations: [tailwind(), vue()]
+  integrations: [
+    tailwind(),
+    vue(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
