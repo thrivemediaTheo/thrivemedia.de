805cdc108c13524c4d90b37e0a2b30bb5f3d50d5
Timo <44401485+Timo972@users.noreply.github.com>
2023-08-13 18:10:20 +0200

    improved sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,42 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+
+const site = "https://thrivemedia.art";
+
+const sitemapx = {
+  index: {
+    changefreq: "monthly",
+    priority: 1,
+  },
+  imprint: {
+    changefreq: "yearly",
+    priority: 0.5,
+  },
+};
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thrivemedia.art",
+  site,
   integrations: [
     tailwind(),
     vue(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap({
+      changefreq: "monthly",
+      lastmod: new Date(),
+      async serialize(item) {
+        const page = item.url.slice(site.length).replace(/\//g, "") || "index";
+        if (!(page in sitemapx)) return item;
+
+        Object.assign(item, sitemapx[page]);
+
+        return item;
+      },
     }),
   ],
 });
