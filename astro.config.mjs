8a10e336e7fd51e53140b01ec06833a410d64d43
Timo <44401485+Timo972@users.noreply.github.com>
2023-08-13 18:13:12 +0200

    prefetch pages on link interaction

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
 const site = "https://thrivemedia.art";
-
 const sitemapx = {
   index: {
     changefreq: "monthly",
@@ -32,11 +32,10 @@ export default defineConfig({
       async serialize(item) {
         const page = item.url.slice(site.length).replace(/\//g, "") || "index";
         if (!(page in sitemapx)) return item;
-
         Object.assign(item, sitemapx[page]);
-
         return item;
       },
     }),
+    prefetch(),
   ],
 });
